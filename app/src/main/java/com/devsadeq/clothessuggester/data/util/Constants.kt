75e01547ff915b8eabce17fc334ba97cdaa365a9
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-04-13 14:52:01 +0300

    create constants

---
+++ app/src/main/java/com/devsadeq/clothessuggester/data/util/Constants.kt
@@ -0,0 +1,14 @@
+package com.devsadeq.clothessuggester.data.util
+
+object Constants {
+    const val SCHEME = "https"
+    const val HOST = "api.openweathermap.org"
+    const val DATA_PATH = "data"
+    const val VERSION_PATH = "2.5"
+    const val WEATHER_PATH = "weather"
+    const val LAT_QUERY = "lat"
+    const val LON_QUERY = "lon"
+    const val UNITS_QUERY = "units"
+    const val APPID_QUERY = "appid"
+    const val STANDARD_UNITS = "standard"
+}
