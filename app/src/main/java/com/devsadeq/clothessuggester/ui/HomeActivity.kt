2e84fb273ad342e1189ccd57723c84bec6bc8f8f
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-04-13 17:18:49 +0300

    change the name of main activity

--- app/src/main/java/com/devsadeq/clothessuggester/ui/HomeActivity.kt
+++ app/src/main/java/com/devsadeq/clothessuggester/ui/HomeActivity.kt
@@ -1,20 +1,21 @@
-package com.devsadeq.clothessuggester
+package com.devsadeq.clothessuggester.ui
 
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.devsadeq.clothessuggester.data.remote.response.WeatherResponse
-import com.devsadeq.clothessuggester.data.remote.service.WeatherApiServiceImpl
+import com.devsadeq.clothessuggester.R
+import com.devsadeq.clothessuggester.data.model.weather.WeatherResponse
+import com.devsadeq.clothessuggester.data.remote.WeatherApiServiceImpl
 import com.devsadeq.clothessuggester.presenter.HomePresenter
 import com.devsadeq.clothessuggester.presenter.IHomeView
 
-class MainActivity : AppCompatActivity(), IHomeView {
+class HomeActivity : AppCompatActivity(), IHomeView {
     private val weatherApiService by lazy { WeatherApiServiceImpl() }
     private val homePresenter by lazy { HomePresenter(this, weatherApiService) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
         homePresenter.getCurrentWeather(35.6892, 51.3890, "metric")
     }
 
